c923f4bd7715cac0ed2ae168feda73e76ea4a1f2
Alex <droppingbucketx@yahoo.com>
2025-04-18 16:30:30 -0400

    Fix bundle path configuration and update Vercel routes

--- my-games/webpack.config.js
+++ my-games/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: 'my-games-bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    publicPath: '/my-games/dist/'
   },
   mode: 'production',
   devtool: 'source-map',
