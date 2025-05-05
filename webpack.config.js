4548860e0a2ac00543bdc92aa94d13c32f31e20c
Alex <droppingbucketx@yahoo.com>
2025-05-05 17:39:39 -0400

    Update React homepage and Firebase configuration

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './'),
     filename: 'homepage_bundle.js',
-    publicPath: '/',
+    publicPath: './',
   },
   module: {
     rules: [
