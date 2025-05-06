456368ad11d701de8780e927fd8a57902ea498f2
Alex <droppingbucketx@yahoo.com>
2025-05-05 20:16:36 -0400

    Use relative publicPath for webpack assets

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'homepage_bundle.js',
-    publicPath: '/',
+    publicPath: '',
     clean: true,
   },
   module: {
